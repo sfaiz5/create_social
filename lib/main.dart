7e1c01b356a5714468b385ef84a808eb52693902
sfaiz15 <107978726+sfaiz15@users.noreply.github.com>
2022-07-05 02:52:06 -0400

    demo

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:create_social/pages/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  //Always needed for firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const SocialApp());
+}
+
+class SocialApp extends StatelessWidget {
+  const SocialApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'SocialApp',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Authentication());
+  }
+}
